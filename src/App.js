ae473d52320edc742558bc0901bd6631e7d67361
MaDiLaa <mauriciodiazlacassy@gmail.com>
2020-05-31 11:04:14 -0300

    Primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LeftSideBar from './animations/LeftSideBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <LeftSideBar/>
+    </div>
+  );
+}
+
+export default App;

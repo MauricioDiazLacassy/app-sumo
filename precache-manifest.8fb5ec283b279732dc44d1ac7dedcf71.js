adc54a68bac31788b9bf60ee8e60dd13d410b959
MaDiLaa <mauriciodiazlacassy@gmail.com>
2020-05-31 11:29:05 -0300

    Updates

---
+++ precache-manifest.8fb5ec283b279732dc44d1ac7dedcf71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e46f2dc3042e1d20bf88cca3be63918",
+    "url": "/app-sumo/index.html"
+  },
+  {
+    "revision": "585f41ccccabe2a5d271",
+    "url": "/app-sumo/static/css/main.a27686f2.chunk.css"
+  },
+  {
+    "revision": "c99aa68ba4da54468e54",
+    "url": "/app-sumo/static/js/2.5fe2207e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-sumo/static/js/2.5fe2207e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585f41ccccabe2a5d271",
+    "url": "/app-sumo/static/js/main.0af12822.chunk.js"
+  },
+  {
+    "revision": "4c995e0d24db54a79e94",
+    "url": "/app-sumo/static/js/runtime-main.782cae06.js"
+  }
+]);

56d1d81b4d315f2d9415482c1c8cdfe8949cb667
MaDiLaa <mauriciodiazlacassy@gmail.com>
2020-06-01 10:14:50 -0300

    Updates

--- precache-manifest.e91abc53c57274570047511b555c28e9.js
+++ precache-manifest.e91abc53c57274570047511b555c28e9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d6301193a4902f62ad8e9d36f8990e40",
+    "revision": "70c43d920d447b658b9ab8e77ddb287c",
     "url": "/app-sumo/index.html"
   },
   {
-    "revision": "6526855741e94eb44c52",
+    "revision": "bac9ed1c5576fff88df0",
     "url": "/app-sumo/static/css/main.747948f4.chunk.css"
   },
   {
-    "revision": "b913d2e0b5d814ce7689",
-    "url": "/app-sumo/static/js/2.352fe4f5.chunk.js"
+    "revision": "93261f4676742cb3b896",
+    "url": "/app-sumo/static/js/2.b268443d.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/app-sumo/static/js/2.352fe4f5.chunk.js.LICENSE.txt"
+    "url": "/app-sumo/static/js/2.b268443d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6526855741e94eb44c52",
-    "url": "/app-sumo/static/js/main.dc6e7431.chunk.js"
+    "revision": "bac9ed1c5576fff88df0",
+    "url": "/app-sumo/static/js/main.5d6b7c7e.chunk.js"
   },
   {
     "revision": "4c995e0d24db54a79e94",
